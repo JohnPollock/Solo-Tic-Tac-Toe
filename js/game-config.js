c27d25cfae9cbd01070af248c432a61d0531000e
John Pollock <solidpollock@yahoo.com>
2025-07-24 08:04:17 -0500

    Initial game
    * Add base game, which can be updated with more features.

---
+++ js/game-config.js
@@ -0,0 +1,18 @@
+(function (window) {
+    'use strict';
+
+    window.gameConfig = {
+        players: {
+            "X": "You",
+            "O": "I",
+            "CAT": "CAT"
+        },
+        levelsRandomizerPcts: {
+            easy: 0.50,
+            medium: 0.80,
+            hard: 0.95
+        }
+    };
+
+})(window);
+
